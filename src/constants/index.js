a6237c47e330957de1ced09392e42e0a9497f113
Jim Vickery <jimrv8@yahoo.com>
2019-01-31 03:45:50 -0600

    added normalized format for live room data

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const HOST = 'http://192.168.1.20:3000'
---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import { HOST } from '../constants';
+
+export function normalizeRooms(rooms) {
+  return rooms.map(room => {
+    return {
+      title: room.listing_name || '',
+      image: `${HOST}${room_image}` || '',
+      homeType: room.home_type || '',
+      bedRoom: room.bed_room || '',
+      instant: room.instant || '',
+    }
+  })
+}
