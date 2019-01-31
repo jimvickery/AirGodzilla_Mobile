de65fd7de11b9463f03812cca75dfd5c124451db
Jim Vickery <jimrv8@yahoo.com>
2019-01-31 04:44:14 -0600

    added room.id to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@ import { HOST } from '../constants';
 export function normalizeRooms(rooms) {
   return rooms.map(room => {
     return {
+      id: room.id || '',
       title: room.listing_name || '',
       image: `${HOST}${room.image}` || '',
       homeType: room.home_type || '',
