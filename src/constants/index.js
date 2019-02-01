51851b078f3c32ba9dcfb19fdec3c95a98c6989c
Jim Vickery <jimrv8@yahoo.com>
2019-01-31 19:00:10 -0600

    added ip address for phone hotspot connections for HOST

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,6 @@
-export const HOST = 'http://192.168.1.20:3000'
+// used for home connections
+// export const HOST = 'http://192.168.1.20:3000'
+
+
+// used for hotspot connections
+export const HOST = 'http://172.20.10.2:3000'
