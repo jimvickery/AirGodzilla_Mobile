0b4076a1cf58485f2f8e12098faaa2c40742c94b
Jim Vickery <jimrv8@yahoo.com>
2019-02-12 20:46:02 -0600

    changed IP address for HOST for home

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 // used for home connections
-// export const HOST = 'http://192.168.1.20:3000'
+export const HOST = 'http://192.168.1.20:3000'
 
 
 // used for hotspot connections
-export const HOST = 'http://172.20.10.2:3000'
+// export const HOST = 'http://172.20.10.2:3000'
 
 // export const HOST = 'http://10.0.2.2:3000'
