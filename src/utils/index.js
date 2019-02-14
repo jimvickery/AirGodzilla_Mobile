f34eaf6cee3ac5f32f6df3793389ad9423ca332e
Jim Vickery <jimrv8@yahoo.com>
2019-02-13 23:12:09 -0600

    save for now

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,3 +13,20 @@ export function normalizeRooms(rooms) {
     }
   })
 }
+
+
+export function normalizeRoom(room) {
+  return {
+    id: room.id || '',
+    title: room.listing_name || '',
+    image: `${HOST}${room.image}` || '',
+    homeType: room.home_type || '',
+    bedRoom: room.bed_room || '',
+    price: room.price || '',
+    instant: room.instant || '',
+    summary: room.summary || '',
+    accomodate: room.accomodate || '',
+    instant: room.instant || '',
+    instant: room.instant || '',
+  }
+}
