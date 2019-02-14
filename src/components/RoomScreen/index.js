658b93cacb0e0cb274eda8f9cea0c5a79c5171aa
Jim Vickery <jimrv8@yahoo.com>
2019-02-13 23:45:51 -0600

    save for now. Getting error on room details page

--- src/components/RoomScreen/index.js
+++ src/components/RoomScreen/index.js
@@ -7,6 +7,8 @@ import {
   Text,
 } from 'react-native';
 
+import { getRoom } from '../../actions/room';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,22 +21,33 @@ const styles = StyleSheet.create({
 });
 
 class RoomScreen extends Component {
+  componentWillMount(){
+    const selectRoomId = this.props.navigation.state.params.item.id;
+    this.props.getRoom(selectRoomId);
+  }
+
   render() {
+    const room = this.props.room;
+
+    if (!room) return null;
+
     const item = this.props.navigation.state.params.item;
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>{item.title}</Text>
+        <Text style={styles.text}>{room.host.name}</Text>
       </View>
     );
   }
 }
 
-const mapStateToProps = state => ({
-
-});
+const mapStateToProps = state => {
+  return ({
+    room: state.room.room,
+  });
+};
 
 const mapDispatchToProps = dispatch => ({
-
+  getRoom: (roomId) => dispatch(getRoom(roomId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RoomScreen);
