a271875b0b70b0ec5e285584c4be1a6f57966082
Jim Vickery <jimrv8@yahoo.com>
2019-02-14 18:58:44 -0600

    added more details ro roomscreen

--- src/components/RoomScreen/index.js
+++ src/components/RoomScreen/index.js
@@ -9,13 +9,14 @@ import {
   Image,
   Dimensions,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import { getRoom } from '../../actions/room';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-
+    marginBottom: 40,
   },
   image: {
     width: Dimensions.get('window').width,
@@ -34,7 +35,29 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 60,
   },
-
+  info: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  about: {
+    paddingBottom: 20,
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e2e2e2'
+  },
+  aboutText: {
+    fontWeight: 'bold',
+  },
+  bookingBar: {
+    position: 'absolute',
+    button: 0,
+    padding: 15,
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderTopColor: 'e2e2e2',
+    backgroundColor: 'white',
+    alikeItems: 'center',
+  },
 });
 
 class RoomScreen extends Component {
@@ -48,19 +71,43 @@ class RoomScreen extends Component {
 
     if (!room) return null;
 
-    const { image, host } = room;
+    const { image, host, bedRoom, bathRoom, accomodate, summary, price } = room;
 
     const item = this.props.navigation.state.params.item;
     return (
-     <ScrollView style = {styles.container}>
-      <Image source = {{uri: image}} style = {styles.image} />
-      <View style = {{padding: 30}}>
-        <View style ={styles.row}>
-          <Text style={{flex: 1}}>{`Hosted by ${host.fullname}`}</Text>
-          <Image source ={{url: host.avatar}} style={styles.avatar} />
-        </View>
-      </View>
-     </ScrollView>
+      <View style={{flex: 1}}>
+        <ScrollView style = {styles.container}>
+          <Image source = {{uri: image}} style = {styles.image} />
+          <View style = {{padding: 30}}>
+
+            <View style ={styles.row}>
+              <Text style={{flex: 1}}>{`Hosted by ${host.fullname}`}</Text>
+              <Image source ={{url: host.avatar}} style={styles.avatar} />
+            </View>
+
+            <View style ={styles.row}>
+              <View style={styles.info}>
+                <Icon name='ios-people-outline' size={40} />
+                <Text>{accomodate} guests(s)</Text>
+              </View>
+
+
+              <View style={styles.info}>
+                <Icon name='ios-alarm-outline' size={40} />
+                <Text>{bedRoom} bedroom(s)</Text>
+              </View>
+
+              <View style={styles.info}>
+                <Icon name='ios-home-outline' size={40} />
+                <Text>{bathRoom} bathroom(s)</Text>
+              </View>
+
+            </View>
+            
+
+          </View>
+        </ScrollView>
+    </View>
     );
   }
 }
