6f1986e644162812f7c79322b73350133d6b8e89
Jim Vickery <jimrv8@yahoo.com>
2019-02-15 20:10:22 -0600

    added GodzillaButton.js

---
+++ src/components/Shared/GodzillaButton.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+
+const styles = StyleSheet.create ({
+  button: {
+    height: 47,
+    alignItmes: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    opacity: 1,
+  },
+  buttonText:   {
+    fontWeight: 'bold',
+    fontSize: 15, 
+  }
+});
+
+export default class GodzillaButton extends Component {
+  render() {
+    const { onpress,
+       label,
+       backgroundColor,
+       textColor, 
+       disabled 
+      } = this.props;
+    
+       return {  
+      <TouchableOpacity
+      style = {[styles.button, {backgroundColor} ]}
+      onpress = { () => onPress() } 
+      disabled = {disabled}>
+      <Text style = {{style.buttonText, {color: textColor }}}>{label}</Text>
+
+      </TouchableOpacity>
+
+    }
+  }
+}
