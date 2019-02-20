5b170d5dfce2990c32e29f7f5173abe81eafeb1c
Jim Vickery <jimrv8@yahoo.com>
2019-02-20 01:19:36 -0600

    added to mainactivity.java on android

--- android/app/src/main/java/com/code4startupreactnativeboilerplate/MainActivity.java
+++ android/app/src/main/java/com/code4startupreactnativeboilerplate/MainActivity.java
@@ -1,7 +1,7 @@
 package com.code4startupreactnativeboilerplate;
 
 import com.facebook.react.ReactActivity;
-
+import android.content.Intent;
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +11,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Code4StartupReactNativeBoilerplate";
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
